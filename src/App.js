03b5d2cebe1b4d9086708d2a48ff9c0996bf107f
mamtabajaj <mamta.bajaj8@gmail.com>
2024-08-13 22:56:16 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,10 +9,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
         <a
           className="App-link"
           href="https://reactjs.org"
